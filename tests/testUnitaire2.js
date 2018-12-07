17f5789d203e1a9f7d2ee63bd4cd6f812dea5cf8
joyet-simon <joyet.simon@hotmail.fr>
2018-12-07 11:31:08 +0100

    refactoring

--- tests/testUnitaire2.js
+++ tests/testUnitaire2.js
@@ -1,14 +1,14 @@
-var assert = require('assert');
+var assert = require("assert");
 
 
-describe ('multiplication', function(){
-    it('vérifier si 3*3 egal 9', function() {
+describe ("multiplication", function(){
+    it("vérifier si 3*3 egal 9", function() {
         assert.equal(3*3,9);
     });
-} )
+} );
 
-describe ('soustraction', function(){
-    it('vérifier si 5-1 egal 4', function() {
+describe ("soustraction", function(){
+    it("vérifier si 5-1 egal 4", function() {
         assert.equal(5-1,4);
     });
-} )
+} );
